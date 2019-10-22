9ce312d22e68d850dec35ff4344677e5b4f1db87
Mirek Kratochvil <exa.exa@gmail.com>
2019-10-22 11:06:46 +0200

    handle null values in embed plot better
    References #19

--- R/02_expr.R
+++ R/02_expr.R
@@ -1,8 +1,9 @@
 
 plotEmbedExpr <- function(e, exprs, cex, alpha) {
+  if(is.null(e)) return()
   par(mar=c(0,0,0,0))
   if(is.null(exprs))
     EmbedSOM::PlotEmbed(e, alpha=alpha, cex=cex, plotf=scattermoreplot, frame.plot=F)
   else
-    EmbedSOM::PlotEmbed(e, data=as.matrix(exprs), value=1, alpha=alpha, cex=cex, plotf=scattermoreplot, frame.plot=F)
+    EmbedSOM::PlotEmbed(e, data=matrix(exprs, ncol=1), value=1, alpha=alpha, cex=cex, plotf=scattermoreplot, frame.plot=F)
 }
