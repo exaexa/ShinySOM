50fb9f2cf21242f89f0ea592f9e48f6a927d2352
Mirek Kratochvil <exa.exa@gmail.com>
2020-01-07 11:21:04 +0100

    improve the pop-size plot a bit

--- R/02_popsizes.R
+++ R/02_popsizes.R
@@ -17,11 +17,13 @@ plotDsAPopSizes <- function(clust, annotation, cellFile, files, showClusters, do
   probs <- apply(probs, 2, function(v) {s <- sum(v); if(s>0) v/s else v;})
 
   if(doReorder && nrow(probs)>1)
-    probs <- probs[,hclust(dist(t(probs)))$order,drop=F]
+    probs <- probs[,hclust(dist(t(probs), method='manhattan'))$order,drop=F]
 
   barplot(probs,
     col=EmbedSOM::ClusterPalette(nlevels(cl))[showIds],
-    border='#808080', ylab='Relative size')
+    border='#808080', ylab='Relative size', xlab='File', xaxt='n',
+    width=1, space=.2)
+  text(.15+1.2*(seq_len(ncol(probs))-1), 0, colnames(probs), srt=90, adj=0)
 }
 
 plotDsAPopLegend <- function(clust, annotation, showClusters) {
